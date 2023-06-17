e2ca64cfb46e18628e6d6b75c77207cf99222938
Edward Xu <edxu96@outlook.com>
2023-06-17 16:53:16 +0200

    Do not write "contents" option, but copy to `files/` (#21)

--- copy.sh
+++ copy.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
-# Copy "README.md" and its associated files to "files/".
-cp ./README.md ./files/
-mkdir -p ./files/img/ && cp ./img/* "$_"
+# The default value, "files/", of the "contents" option can be overriden, then
+# "files/" will not be included. So, we do not specify the value of the
+# "contents" option, but copy to-be-included files to "files/" directory, which
+# is done by this script.
+
+# "README.md" and its associated files.
+cp README.md files/
+mkdir -p files/img/
+cp img/* "$_"
+
+# Some simple "pandapower" tutorials.
+mkdir -p files/pandapower/
+cp pandapower/tutorials/create_*.ipynb files/pandapower/
+
+# Some "pandapower" tutorials for 3-phase ACPF.
+mkdir -p files/pandapower/3ph
+cp pandapower/tutorials/{ieee_european_lv_*,three_phase_loadflow_tutorial_simple}.ipynb files/pandapower/3ph
